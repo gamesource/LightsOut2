fb18e4603a7c9418f497dd968d868930ea603d9d
gamesource <mr.gamesource@gmail.com>
2015-05-19 23:18:34 +0300

    Gridd class

--- app/src/main/java/random/lightsout/MainActivity.java
+++ app/src/main/java/random/lightsout/MainActivity.java
@@ -1,17 +1,55 @@
 package random.lightsout;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.*;
+import android.widget.*;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+
+    private final int MAX_ROW = 5;
+    private final int MAX_COLUMN = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+     /*   TableLayout tableLayout = (TableLayout) findViewById(R.id.table_layout_id);
+
+
+        for (int row = 0; row < MAX_ROW; row++) {
+
+            TableRow tableRow = new TableRow(this);
+            tableLayout.addView(tableRow);
+
+            for (int column = 0; column < MAX_COLUMN; column++) {
+
+                final ToggleButton toggleButton = new ToggleButton(this);
+                toggleButton.setText(row + "x" + column);
+                toggleButton.setTextOff("");
+                toggleButton.setTextOn("");
+                toggleButton.setTag(row + "," + column);
+                tableRow.addView(toggleButton);
+
+            }
+        }
+
+        ToggleButton view = (ToggleButton)tableLayout.findViewWithTag("1,3");
+        view.setTextOn("Changed!");
+*/
+
+
+
+
+
+
+
+
+
+
+
     }
 
     @Override
---
+++ app/src/main/java/random/model/Grid.java
@@ -0,0 +1,7 @@
+package random.model;
+
+/**
+ * Created by gamer on 19.05.2015.
+ */
+public class Grid {
+}
